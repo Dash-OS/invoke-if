eeef6d63649bc8991f3d7728cfefc264ac67cc4a
bradynapier <brady.napier@dashos.net>
2017-05-29 08:53:32 +0000

    curry return values

--- src/main.js
+++ src/main.js
@@ -1,7 +1,28 @@
+const invokeNext = (response, ...tests) => tests.forEach(
+  ([ test, fn, ...rest]) => {
+    if ( typeof test === 'function' ) { test = test(response) }
+    if ( test ) { 
+      const next = typeof fn === 'function'
+        ? fn(response) 
+        : fn
+      if ( rest.length > 0 ) {
+        return invokeNext(next, ...rest) 
+      } else { return next }
+    }
+  }
+)
+
 const invokeIf = (...tests) => tests.forEach(
   ([ test, fn, ...rest ]) => {
     if ( typeof test === 'function' ) { test = test() }
-    if ( test ) { fn() ; rest.length > 0 && invokeIf(...rest) }
+    if ( test ) { 
+      const response = typeof fn === 'function'
+        ? fn()
+        : fn
+      if ( rest.length > 0 ) {
+        return invokeNext(response, ...rest)
+      } else { return response }
+    }
   }
 )
 
